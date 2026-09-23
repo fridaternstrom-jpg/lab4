db4f60a1c678c9b5b115a11e12ca49261f00a55b
Frida Ternström <frida.ternstrom@gmail.com>
2026-09-23 15:34:14 +0200

    provar att göra en funktion för uppgift 7

--- uppgift7.js
+++ uppgift7.js
@@ -5,3 +5,17 @@ Uppgiften */
 
 let numbers = [5, 9, 1, 3, 7, 4];
 
+
+function sumOfNumbers(numbers) {
+let sum = 0;                        //Ett standardvärde att utgå ifrån
+
+    numbers.forEach(number => {     //standardsum är lika med varje enskilt värde i arrayen + 0
+        sum = number + sum;
+    });
+    
+    return sum * numbers.length;       //summan, var och en från arrayen, gånger längden på arrayen
+}
+
+console.log(sumOfNumbers(numbers));     //anropar funktionen
+
+
