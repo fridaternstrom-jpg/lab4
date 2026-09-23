64dd3744d7cc20f9f64276793715cae3eb0835cb
Frida Ternström <frida.ternstrom@gmail.com>
2026-09-23 13:59:40 +0200

    gör uppgift 5, skapar array och modifierar den

---
+++ uppgift5.js
@@ -0,0 +1,19 @@
+/*Lösning till uppgift 5 av Frida Ternström, 2026
+Nu gör jag en array och modifierar den på olika sätt enligt
+instruktionerna*/
+
+"use strict";
+
+let food = ["pannkakor", "hamburgare", "soppa", "kotlett", "godis"];
+
+console.log(food);
+
+console.log(food[0]);
+
+console.log(food[4]);
+
+food.push("julskinka");
+
+food.shift([0]);
+
+console.log(food);
