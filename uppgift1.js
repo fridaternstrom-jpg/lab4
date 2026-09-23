849ca89b782802b31edf2be5304cadceb3859548
Frida Ternström <frida.ternstrom@gmail.com>
2026-09-23 12:16:11 +0200

    skapar variabler och skriver ut dem i uppgift 1

--- uppgift1.js
+++ uppgift1.js
@@ -1,2 +1,17 @@
+/*Lösning till uppgift 1 av Frida Ternström, 2026
+Uppgiften innefattar att jag skapar variabler och sedan 
+låter dem skrivar ut till konsollen*/
+
 "use strict";
 
+const firstName = "Frida";
+
+const lastName = "Ternström";
+
+const age = 39;
+
+const isStudent = true;
+
+console.log(`${firstName} ${lastName}
+Ålder: ${age}
+Student: ${isStudent}`);
